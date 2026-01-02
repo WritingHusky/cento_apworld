23064f8a9ff00b9eca10974ce601d2ca1a92ad22
WritingHusky <115745794+WritingHusky@users.noreply.github.com>
2026-01-02 14:22:18 -0800

    Basic Setup but it won't load

--- __init__.py
+++ __init__.py
@@ -1,3 +1,4 @@
+from typing import ClassVar
 from worlds.generic.Rules import exclusion_rules
 from BaseClasses import Region, Entrance, Tutorial, Item
 from worlds.AutoWorld import World, WebWorld
@@ -9,6 +10,9 @@ from worlds.LauncherComponents import (
     launch as launch_component,
     Type,
 )
+from .Items import ITEM_TABLE, CentoItem
+from .Locations import LOCATION_TABLE, CentoLocation
+from .options import CentoOptions
 
 
 def launch_client(*args: str):
@@ -17,10 +21,11 @@ def launch_client(*args: str):
     launch_component(launch, name="CentoClient", args=args)
 
 
+print("test")
+
 components.append(
     Component(
         "Cento Client",
-        "CentoClient",
         func=launch_client,
         component_type=Type.CLIENT,
     )
@@ -36,37 +41,52 @@ class CentoWeb(WebWorld):
             "English",
             "setup_en.md",
             "setup/en",
-            ["Mewlif"],
+            ["Hash"],
         )
     ]
 
 
 class CentoWorld(World):
     """
-    cento is an RPG where every choice you make matters. You could choose to hurt all the enemies, eventually
-    causing genocide of the monster species. Or you can spare all the enemies, befriending them and freeing them
-    from their underground prison.
+    Cento is a rythm game
     """
 
-    game = "cento"
-    options_dataclass = centoOptions
-    options: centoOptions
-    web = centoWeb()
+    game = "Cento"
+    options_dataclass = CentoOptions
+    options: CentoOptions
+    web: ClassVar[CentoWeb] = CentoWeb()
 
-    item_name_to_id = {name: data.code for name, data in item_table.items()}
-    location_name_to_id = {name: data.id for name, data in advancement_table.items()}
+    item_name_to_id = {name: data.code for name, data in ITEM_TABLE.items()}
+    location_name_to_id = {name: data.code for name, data in LOCATION_TABLE.items()}
+
+    item_pool: list[CentoItem] = []
 
     def get_filler_item_name(self):
-        pass
+        return "test"
 
     def create_items(self):
-        pass
+        data = ITEM_TABLE["test"]
+        self.item_pool.append(CentoItem("test", self.player, data))
+
+        for item in self.item_pool:
+            self.multiworld.itempool.append(item)
+        return
 
     def set_rules(self):
         pass
 
     def create_regions(self):
-        pass
+        menu_region = Region("Menu", self.player, self.multiworld)
+        self.multiworld.regions.append(menu_region)  # or use += [menu_region...]
+
+        main_region = Region("Main Area", self.player, self.multiworld)
+
+        main_region.locations.append(
+            CentoLocation(self.player, "test", LOCATION_TABLE["test"])
+        )
+        self.multiworld.regions.append(main_region)
+
+        menu_region.connect(main_region)
 
     def fill_slot_data(self):
         return self._get_cento_data()
@@ -75,6 +95,6 @@ class CentoWorld(World):
         return {}
 
     def create_item(self, name: str) -> Item:
-        item_data = item_table[name]
-        item = centoItem(name, item_data.classification, item_data.code, self.player)
+        item_data = ITEM_TABLE[name]
+        item = CentoItem(name, item_data.classification, item_data.code, self.player)
         return item
